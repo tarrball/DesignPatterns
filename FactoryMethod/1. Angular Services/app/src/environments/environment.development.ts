0700af1098df0fdcec2daabd7b37e3f61944bc7c
Andrew Tarr <atarr@dontpaniclabs.com>
2023-01-29 15:03:53 -0600

    Dropping Angular factory method idea

--- FactoryMethod/1. Angular Services/app/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiUrl: 'https://dev.base.com/api',
-  production: false,
-};
--- FactoryMethod/1. Angular Services/app/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiUrl: 'https://base.com/api',
-  production: true,
-};
